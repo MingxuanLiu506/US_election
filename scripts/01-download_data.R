231dcee22968f91b934be361caffd209fdb2ed5e
MingxuanLiu506 <mingxuan.liu@mail.utoronto.ca>
2024-10-22 01:44:07 -0400

    Add files via upload

---
+++ scripts/01-download_data.R
@@ -0,0 +1,27 @@
+poll<-read.csv(file = "F:/president_polls.csv")#### Preamble ####
+# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Download data ####
+# [...ADD CODE HERE TO DOWNLOAD...]
+
+poll<-read.csv(file = "starter_folder-main/data/01-raw_data/president_polls.csv")
+
+#### Save data ####
+# [...UPDATE THIS...]
+# change the_raw_data to whatever name you assigned when you downloaded it.
+
+write_csv(poll, "starter_folder-main/data/01-raw_data/raw_data.csv") 
+
+
